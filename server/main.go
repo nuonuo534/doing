43a6b2df7cba32027ba9b9d9e4cdf713325ff73c
yanxinhong <597057534@qq.com>
2021-03-11 17:45:12 +0800

    feat:加上守护进程

--- server/main.go
+++ server/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"server/algorithm"
 	"server/routers"
@@ -10,12 +15,11 @@ import (
 )
 
 func main() {
+
 	arr := []int{6, 7, 2, 34, 5, 8, 9, 1}
 	algorithm.BubbleSort(arr)
 	fmt.Printf("arr : %v\n", arr)
-
 	router := routers.InitRouter()
-
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
 		Handler:        router,
@@ -23,5 +27,20 @@ func main() {
 		WriteTimeout:   setting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
-	s.ListenAndServe()
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Printf("Listen: %s\n", err)
+		}
+	}()
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Printf("Shutdown Server ...\n")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+
 }
